556e61d513846628e29c7ac2e94196b8596612ac
daiqiang <342671940@QQ.COM>
2014-12-18 10:19:49 +0800

    001
    使用dwr实现后台推送功能的DEMO

---
+++ src/com/huatech/messageremind/service/MessagePush.java
@@ -0,0 +1,21 @@
+package com.huatech.messageremind.service;
+
+import javax.servlet.ServletException;
+import org.directwebremoting.*;
+
+public class MessagePush {
+	public void onPageLoad() {
+		
+       //ScriptSession scriptSession = WebContextFactory.get().getScriptSession();
+       //scriptSession.setAttribute(userId, userId);
+		/*
+       DwrScriptSessionManagerUtil dwrScriptSessionManagerUtil = new DwrScriptSessionManagerUtil();
+       try {
+              dwrScriptSessionManagerUtil.init();  
+              System.out.println("dwrScriptSessionManagerUtil初始化成功");
+       } catch (ServletException e) {
+              e.printStackTrace();
+       }
+       */
+	}
+}
---
+++ src/com/huatech/messageremind/service/User.java
@@ -0,0 +1,35 @@
+package com.huatech.messageremind.service;  
+/** 
+ * <p>项目名称：dwrFirst </p>
+ * <p>文件名称：User.java </p>
+ * <p>包   名  称：com.huatech.messageremind.service <p>
+ * <p>内容摘要: TODO(用一句话描述该文件做什么)</p> 
+ * <p>创   建  人：daiqiang</p>
+ * <p>创建时间: 2014-11-24 下午11:46:09 </p>
+ * <p>修改记录： </p>
+ * <pre>
+ * 修改日期：2014-11-24 下午11:46:09
+ * 版  本   号：
+ * 修  改   人：daiqiang
+ * 修改内容：
+ * </pre>
+ */
+public class User {
+	
+	private String humanid;
+	private String password;
+	
+	public String getHumanid() {
+		return humanid;
+	}
+	public void setHumanid(String humanid) {
+		this.humanid = humanid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
+  
